9a3ea409d824708e64de907d17a8b684926d759c
Dan Almenar <danielalmenar@gmail.com>
2024-01-06 18:46:10 -0500

    ADD: navbar base styling

--- tg-photo-viewer/src/app/page.tsx
+++ tg-photo-viewer/src/app/page.tsx
@@ -10,11 +10,13 @@ export default function Home() {
 		],
 	}
   return (
-    <main className={styles.main}>
+	  <>
 		<Navbar data={props.data} />
+    <main className={styles.main}>
 		<h1>Hello, World!</h1>
 		&nbsp;
 		<span className={`material-icons-outlined ${styles.heart}`}>favorite_border</span>
     </main>
+	  </>
   )
 }
--- tg-photo-viewer/src/components/layout/navbar/Navbar.tsx
+++ tg-photo-viewer/src/components/layout/navbar/Navbar.tsx
@@ -1,13 +1,14 @@
 import { NavbarProps } from '../../../customTypes/types.ts';
+import styles from './navbar.module.css';
 
 const Navbar: React.FunctionComponent<NavbarProps> = (navprops: NavbarProps): React.JSX.Element => {
 	const { data } = navprops;
 	return (
-		<nav>
-			<ul>
+		<nav className={styles.nav}>
+			<ul className={styles.ul}>
 				{ data.map((item) => (
-					<li key={item.id}>
-					<a className="material-icons" href={item.url}>{item.label}</a>
+					<li className={styles.li} key={item.id}>
+					<a className={`material-icons ${styles.a}`} href={item.url}>{item.label}</a>
 					</li>
 					)) 
 				}
