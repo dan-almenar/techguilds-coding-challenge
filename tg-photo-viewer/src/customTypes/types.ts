88fdf9b1b763cc2862c7e048f7e6ac43c78bcb62
Dan Almenar <danielalmenar@gmail.com>
2024-01-13 15:10:01 -0500

    ADD: imagesGrid - start

--- tg-photo-viewer/src/customTypes/types.ts
+++ tg-photo-viewer/src/customTypes/types.ts
@@ -5,3 +5,30 @@ export interface NavbarProps {
 export interface FooterProps {
 	data: { year: number, url: string, label: string, target?: Target };
 };
+export interface ImageData {
+	id: string,
+	width: number,
+	height: number,
+	description: string,
+	alt_description: string,
+	location: { [key: string]: string },
+	urls: {
+		full: string,
+		regular: string,
+		small: string
+	},
+	links: {
+		html: string,
+		download: string
+	},
+	likes: number,
+	user: {
+		id: string,
+		username: string,
+		name: string,
+		portfolio_url: string
+	}
+};
+export interface RequestOptions {
+	headers?: { [key: string]: string };
+};
