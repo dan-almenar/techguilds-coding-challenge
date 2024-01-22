fe01bf12c3ab1601aa6b18291f8d18b52e4b1a1c
Dan Almenar <danielalmenar@gmail.com>
2024-01-22 12:16:57 -0500

    EDIT: about page

--- tg-photo-viewer/src/pages/about/index.tsx
+++ tg-photo-viewer/src/pages/about/index.tsx
@@ -1,13 +1,18 @@
 import styles from './about.module.css';
 import LayoutComponent from '../../components/layout/layout/LayoutComponent';
+import Headers from '../../components/layout/headers/Headers';
+import HeaderProps from '../../customTypes/types.ts';
 
 const About: React.FunctionComponent = (): React.JSX.Element => {
-	const title: string = 'About';
+	const props: HeaderProps = {
+		title: 'About',
+		description: 'About the app',
+	}
 	return (
 		<>
-			<title>{title}</title>
+			<Headers {...props} />
 			<LayoutComponent>
-				<h1 className={styles.h1}>About</h1>
+				<h1 className={styles.h1}>About the App</h1>
 			</LayoutComponent>
 		</>
 	);
