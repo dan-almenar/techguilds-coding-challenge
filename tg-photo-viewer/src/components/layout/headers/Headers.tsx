e63b08df5f81ff27b5f7d97d73b8b2afcf56d5e4
Dan Almenar <danielalmenar@gmail.com>
2024-01-22 12:16:15 -0500

    ADD: Headers component

---
+++ tg-photo-viewer/src/components/layout/headers/Headers.tsx
@@ -0,0 +1,18 @@
+import Head from 'next/head';
+import HeaderProps from '../../../customTypes/types.ts';
+
+const Headers: React.FunctionComponent = (props: HeaderProps): React.JSX.Element => {
+	const { title, description } = props;
+	
+	return (
+		<Head>
+			<title>{title}</title>
+			<meta name="description" content={description} />
+			{ /* Google Material Icons */ }
+			<link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons|Material+Icons+Outlined"/>
+		</Head>
+	);
+	
+}
+
+export default Headers;
