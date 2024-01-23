d33e97b967da39a74e487ebec822bf4b3399b503
Dan Almenar <danielalmenar@gmail.com>
2024-01-23 10:07:26 -0500

    EDIT: About page styling

--- tg-photo-viewer/src/components/layout/headers/Headers.tsx
+++ tg-photo-viewer/src/components/layout/headers/Headers.tsx
@@ -5,12 +5,14 @@ const Headers: React.FunctionComponent = (props: HeaderProps): React.JSX.Element
 	const { title, description } = props;
 	
 	return (
-		<Head>
-			<title>{title}</title>
-			<meta name="description" content={description} />
+		<>
+			<Head>
+				<title>{title}</title>
+				<meta name="description" content={description} />
+			</Head>
 			{ /* Google Material Icons */ }
 			<link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons|Material+Icons+Outlined"/>
-		</Head>
+		</>
 	);
 	
 }
