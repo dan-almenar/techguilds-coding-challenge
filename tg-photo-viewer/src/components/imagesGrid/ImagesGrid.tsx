d7e41b56ebf6f3bc07a141fc0cd4e8b761af2c28
Dan Almenar <danielalmenar@gmail.com>
2024-01-29 09:22:02 -0500

    FIX: child declaration - ImagesGrid

--- tg-photo-viewer/src/components/imagesGrid/ImagesGrid.tsx
+++ tg-photo-viewer/src/components/imagesGrid/ImagesGrid.tsx
@@ -1,12 +1,13 @@
 import { ImageData } from '../../customTypes/types.ts';
 import styles from './imgGrid.module.css';
+import ImageComponent from '../imageComponent/ImageComponent.tsx';
 
 const ImageGrid: React.FunctionComponent<ImageData[]> = (props: ImageData[]): React.JSX.Element => {
 	return (
 		<div id="img-grid" className={styles.imgGrid}>
 		{
 			props.map((imgData: ImageData) => (
-				<ImageComponent key={imgData.id} props={imgData} />
+				<ImageComponent key={imgData.id} {...imgData} />
 			))
 		}
 		</div>
