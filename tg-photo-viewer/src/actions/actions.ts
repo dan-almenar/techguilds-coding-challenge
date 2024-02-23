c8020cc948f3b16ff51724de626eba9cbcea1bee
Dan Almenar <danielalmenar@gmail.com>
2024-02-23 12:35:22 -0500

    FIX: fetch images helper functions

--- tg-photo-viewer/src/actions/actions.ts
+++ tg-photo-viewer/src/actions/actions.ts
@@ -1,8 +1,8 @@
 "use server"
 import { RequestOptions, ImageData } from "../customTypes/types";
 
-const UNSPLASH_ENDPOINT_MULTIPLE: string = 'https://api.unsplash.com/photos/';
-const UNSPLASH_ENDPOINT_SINGLE: string = 'https://api.unsplash.com/photos/random';
+const UNSPLASH_ENDPOINT_BASE: string = 'https://api.unsplash.com/photos/';
+const UNSPLASH_ENDPOINT_RANDOM: string = 'https://api.unsplash.com/photos/random';
 const defaultOptions: RequestOptions = {
 	headers: {
 		'Accept-Version': 'v1',
@@ -16,18 +16,15 @@ const baseParams: Record<string, string | number> = {
 	'count': 12,
 }
 
-const parseURI: Function = (uri: string, params?: Record<string, string | number>, page: number = 1): string => {
-	const endpoint: URL = new URL(uri);
+const parseURI: Function = (
+	params: Record<string, string | number> = baseParams
+): string => {
+	const endpoint: URL = params.query ? new URL(UNSPLASH_ENDPOINT_RANDOM) : new URL(UNSPLASH_ENDPOINT_BASE);
 
-	endpoint.searchParams.append('page', page.toString());
-
-	if (params) {
-		for (const [key, value] of Object.entries(params)) {
-			endpoint.searchParams.append(key, value.toString());
-		};
+	for (const [key, value] of Object.entries(params)) {
+		endpoint.searchParams.append(key, value.toString());
 	};
 
-	console.log(endpoint.href);
 	return endpoint.href;
 }
 
@@ -59,21 +56,20 @@ const toImageData: Function = (item: any): ImageData => {
 };
 
 async function fetchImages(
-	endpoint: string = UNSPLASH_ENDPOINT_SINGLE,
 	options: RequestOptions = defaultOptions,
 	params: Record<string, string | number> = baseParams,
-	query: string = ""
 ): Promise<ImageData[]> {
 	try {
-		let uri: string = parseURI(endpoint, params);
-		query && (uri += `&query=${query}`);
+		let uri: string = parseURI(params);
 
 		const response: Response = await fetch(uri, options);
 		if (response.ok){
 			let data: ImageData[] = await response.json();
 			data = data.map((item: any) => toImageData(item));
 			return data;
-		};
+		} else {
+			return Promise.reject(response);
+		}
 	} catch(err) {
 		return Promise.reject(err);
 	}
