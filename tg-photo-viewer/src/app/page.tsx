b8d23b6287f2b2727deb74b1db604308f3cc8719
Dan Almenar <danielalmenar@gmail.com>
2024-03-08 15:08:17 -0500

    EDIT: page.tsx - fit new API

--- tg-photo-viewer/src/app/page.tsx
+++ tg-photo-viewer/src/app/page.tsx
@@ -1,20 +1,18 @@
 import LayoutComponent from '../components/layout/layout/LayoutComponent.tsx';
 import SearchBar from '../components/searchbar/SearchBar.tsx';
 import ImagesGrid from '../components/imagesGrid/ImagesGrid.tsx';
-import { fetchData } from '../modules/actions.ts';
+import { getImages } from '../modules/actions.ts';
 import type { ImageData } from '../customTypes/types.ts';
 
 export default async function Home() {
-	const params: Record<string, string | number> = {
-		page: 1,
-		per_page: 12, // per_page param required when no query is provided
-		count: 16, // count param required when a query is provided
-	};
-	const data: ImageData[] = await fetchData(params);
+	/* first images are fetched on the server
+	 * using the default parameters from the constants.ts file
+	*/
+	const images: ImageData[] = await(getImages());
 	return (
 		< LayoutComponent>
 			<SearchBar />
-			<ImagesGrid data={data}/> 
+			<ImagesGrid data={images}/> 
 		</LayoutComponent>
 	)
 };
